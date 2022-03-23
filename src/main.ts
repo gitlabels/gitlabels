e9d13e7928ee357b237b83f6a751a2e17a7c8ba5
Jozef Izso <jizso@slido.com>
2022-03-23 21:42:30 +0100

    Update labels based on labels.json definition

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,55 @@
+import { env } from "process";
+
 const core = require('@actions/core');
+const { Octokit } = require("@octokit/rest");
+const fs = require('fs');
+const { readFile } = require('fs/promises');
+
+interface LabelUpdate {
+  name: string;
+  color: string;
+  description?: string;
+}
 
 async function run() {
   try {
-    
+    const owner = env.GITHUB_REPOSITORY_OWNER || ''
+    const repo = env.GITHUB_REPOSITORY?.substring(owner.length + 1)
+
+    const octokit = new Octokit({
+      auth: env.GITHUB_TOKEN,
+      userAgent: 'gitlabels-action',
+    })
+
+    const labelsPath = '.github/labels.json';
+    if (!fs.existsSync(labelsPath)) {
+      return;
+    }
+
+    let content = await readFile(labelsPath);
+    let labels = JSON.parse(content);
+
+    if (labels !instanceof Array) {
+      core.setFailed('labels.json file must contain array of label definitions.')
+      return
+    }
+
+    labels.forEach(async (newLabel: LabelUpdate) => {
+      var name = newLabel.name;
+      var color = newLabel.color;
+      var description = newLabel.description;
+
+      await octokit.rest.issues.updateLabel({
+        owner,
+        repo,
+        name,
+        new_name: name,
+        color,
+        description
+      });
+
+      core.info(`Label ${name} updated.`)
+    });
   }
   catch (error) {
     if (error instanceof Error) {
